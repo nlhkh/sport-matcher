5c508124620f0f154f8b2284697896c96fb591ad
Kha <nlhkha@gmail.com>
2022-12-06 01:00:43 +0200

    remove all prisma and supabase dependencies (#1)

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,7 @@
 <template>
-    <div v-if="data.length === 0"></div>
     <div>Upcoming games</div>
-    {{ data }}
 </template>
 
 <script lang="ts" setup>
-import { Game } from '@prisma/client';
-
-definePageMeta({
-    middleware: ["auth"]
-})
-
-const { data } = useFetch<Game[]>("/api/upcomingGames", {
-    headers: useRequestHeaders(["cookie"])
-})
 
 </script>
